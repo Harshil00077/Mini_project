400f490779900cf92ffeff4c72bd66fcdd4813b9
Harshil00077 <155606208+Harshil00077@users.noreply.github.com>
2024-02-19 14:30:31 +0530

    Update calculator.py
    added sqrt,factorial,power and logarithmic functions

--- calculator.py
+++ calculator.py
@@ -1,26 +1,66 @@
 #! /usr/bin/python3
 
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
+import math
 
-a = float(input("Enter First Number: "))
-b = float(input("Enter Second Number: "))
-ch = input("Enter the Choice: ")
 
-if ch == '1':
-    res = a + b
-elif ch == '2':
-    res = a - b
-elif ch == '3':
-    res = a * b
-elif ch == '4':
-    if b != 0:
-        res = a / b
+def fact(n):
+    if n==0 or n==1:
+        return 1
+    return n*fact(n-1)
+
+while(True):
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Square Root")
+    print("6. Factorial")
+    print("7. Natural Logarithm")
+    print("8. Power")
+    print("9- Exit")
+
+    ch = input("Enter the Choice: ")
+
+    if ch == '1':
+        a = float(input("Enter First Number: "))
+        b = float(input("Enter Second Number: "))
+        res = a + b
+    elif ch == '2':
+        a = float(input("Enter First Number: "))
+        b = float(input("Enter Second Number: "))
+        res = a - b
+    elif ch == '3':
+        a = float(input("Enter First Number: "))
+        b = float(input("Enter Second Number: "))
+        res = a * b
+    elif ch == '4':
+        a = float(input("Enter First Number: "))
+        b = float(input("Enter Second Number: "))
+        if b != 0:
+            res = a / b
+        else:
+            res = "Cannot divide by zero"
+    elif ch == '5':
+        a = float(input("Enter The Number: "))
+        res = a**0.5
+    elif ch == '6':
+        a = float(input("Enter The Number: "))
+        if a>=0:
+            res = fact(a)
+        res = "Factorial of negative numbers is not defined."
+    elif ch == '7':
+        a = float(input("Enter First Number: "))
+        if b > 0:
+            res = math.log(a)
+        res = "Cannot take log of negative or zero"
+    elif ch == '8':
+        a = float(input("Enter The Base for power: "))
+        b = float(input("Enter Power of Numoube You Want: "))
+        res = a**b
     else:
-        res = "Cannot divide by zero"
-else:
-    res = "Invalid Choice"
-
-print("Result:", res)
+        res = "Exiting"
+    
+    if res!="Exiting":
+        print("Result:", res)
+    else:
+        break
